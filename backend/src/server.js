7a2e6fe3981535870a5e1101e842ffa712cc08dd
darthcoder92 <hanunsreedhar@gmail.com>
2026-01-15 16:34:33 +0530

    ready to deploy 16

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,11 +18,13 @@ app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    const frontendPath = path.join(process.cwd(), "frontend", "dist");
+    app.use(express.static(frontendPath));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-    })
+        res.sendFile(path.join(frontendPath,"index.html"));
+    });
 }
 
 // IMPORTANT: Export for Vercel
